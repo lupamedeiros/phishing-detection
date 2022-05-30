eb5be8ddb9d192b2a74a9b27b1ff0178a3709be5
Luiz Paulo <luizpaulo13@gmail.com>
2022-05-29 21:42:44 -0300

    update main.py

--- source/main.py
+++ source/main.py
@@ -1,4 +1,9 @@
-import numpy as np
-import panda as pd
+from fastapi import FastAPI
 
+# Instantiate the app.
+app = FastAPI()
 
+# Define a GET on the specified endpoint.
+@app.get("/")
+async def say_hello():
+    return {"greeting": "Hello World!"}
